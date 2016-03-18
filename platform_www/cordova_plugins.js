903942e78813f3e1562b6732e087be4832657244
UnderSampled <undersampled@live.com>
2016-03-18 01:07:48 +0100

    initial commit

---
+++ platform_www/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-vibration/www/vibration.js",
+        "id": "cordova-plugin-vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.1",
+    "cordova-plugin-crosswalk-webview": "1.6.0",
+    "cordova-plugin-vibration": "2.1.0"
+};
+// BOTTOM OF METADATA
+});
